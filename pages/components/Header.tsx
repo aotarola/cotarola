1978ff557820241efb739a64ef7abb968ead06d2
aotarola <aotarola@groupon.com>
2021-02-28 17:19:25 -0800

    typofix

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: FunctionComponent = () => {
           <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
             <h1 className="my-4 text-5xl font-bold leading-tight">Claudia Otárola</h1>
             <p className="font-veryberry leading-normal text-4xl mb-8">
-              Mentor en autosanación espiritual
+              Mentora en autosanación espiritual
             </p>
           </div>
           <div className="w-full md:w-3/5 py-6 text-center">
