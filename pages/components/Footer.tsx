e21a86d0e4f575cd0457735a52c326e12b1042ac
Andres Otarola <andres@otarola.me>
2021-02-28 16:57:05 -0800

    add social links

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent } from 'react'
 
 const Footer: FunctionComponent = () => {
   return (
@@ -86,7 +86,7 @@ const Footer: FunctionComponent = () => {
             <ul className="list-reset mb-6">
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
-                  href="#"
+                  href="http://facebook.com/clauotarola"
                   className="no-underline hover:underline text-gray-800 hover:text-pink-500"
                 >
                   Facebook
@@ -94,18 +94,42 @@ const Footer: FunctionComponent = () => {
               </li>
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
-                  href="#"
+                  href="https://www.instagram.com/clau__otarola/"
                   className="no-underline hover:underline text-gray-800 hover:text-pink-500"
                 >
-                  Linkedin
+                  Instagram
                 </a>
               </li>
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
-                  href="#"
+                  href="https://www.youtube.com/channel/UCL9mBa9UXELydAtdOPctiaQ"
+                  className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                >
+                  Youtube
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://twitter.com/clau__otarola"
                   className="no-underline hover:underline text-gray-800 hover:text-pink-500"
                 >
                   Twitter
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://vm.tiktok.com/ZS9Uq8sQ/"
+                  className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                >
+                  Tiktok
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://wa.me/56932752284"
+                  className="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                >
+                  Whatsapp
                 </a>
               </li>
             </ul>
@@ -142,7 +166,7 @@ const Footer: FunctionComponent = () => {
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
