e1c8cc48c57bedff801ed3a573464f658ed83336
aotarola <aotarola@groupon.com>
2021-03-02 17:03:13 -0800

    add networkstate and visual activity component

--- api.js
+++ api.js
@@ -1,17 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
+const Mailchimp = require('mailchimp-api-v3')
+
+const { MAILCHIMP_API_KEY } = process.env
+
+const mailchimp = new Mailchimp(MAILCHIMP_API_KEY)
 
 const corsOptions = {
   origin: ['https://dev.claudiaotarola.com', 'https://claudiaotarola.com'],
 }
 
+const jsonParser = bodyParser.json()
+
 express()
-  .use(bodyParser.json())
+  .use(jsonParser)
   .use(cors(corsOptions))
-  .get('/', (req, res) => {
-    res.write('boo')
+  .post('/', async (req, res) => {
+    const resp = await mailchimp.request({
+      method: 'get',
+      path: 'lists',
+    })
+
+    res.write(JSON.stringify(resp))
     res.end()
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
--- pages/components/ContactMe.tsx
+++ pages/components/ContactMe.tsx
@@ -1,30 +1,111 @@
-import { FunctionComponent, useState } from 'react'
+import { ChangeEvent, FunctionComponent, FormEvent, useState } from 'react'
 
-const options = [
-  {
-    id: 'abre-tus-alas',
-    name: 'Abre tus alas',
-  },
-  {
-    id: 'poder-interior',
-    name: 'Reconocimiento de tu Poder Interior',
-  },
-  { id: 'yoga', name: 'Clases de Yoga' },
-]
+const HEROKU_BASEURL = 'somewhere'
+
+type NetworkNotAsked = {
+  state: 'notAsked'
+}
+
+type NetworkLoading = {
+  state: 'loading'
+}
+
+type NetworkError = {
+  state: 'error'
+  code: number
+}
+
+type NetworkSuccess = {
+  state: 'success'
+  data: {
+    status: string
+  }
+}
+
+type NetworkState = NetworkNotAsked | NetworkLoading | NetworkError | NetworkSuccess
 
 const ContactMe: FunctionComponent = () => {
+  const [currentName, setName] = useState(null)
+  const [currentEmail, setEmail] = useState(null)
   const [currentOption, setOption] = useState(null)
-  const yogaOptionActive = currentOption === 'yoga'
+  const [networkState, setNetworkState] = useState<NetworkState>({ state: 'notAsked' })
 
-  const optionClicked = (event): void => {
-    setOption((prev) => event.target.value)
+  // TODO: Error message, maybe have 4 states: not asked, loaded, failure, success
+  const optionSelected = (event): void => {
+    setOption((_) => event.target.value)
   }
 
-  const doAction = (): void => {
-    if (currentOption && currentOption !== 'yoga') {
-      window.location.href = 'https://calendly.com/claudiaotarola'
+  const doSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault()
+    if (currentOption && currentEmail && currentName) {
+      try {
+        const response = await fetch(HEROKU_BASEURL, {
+          method: 'post',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: currentName,
+            email: currentEmail,
+            option: currentOption,
+          }),
+        })
+
+        const data = await response.json()
+        if (data.ok) {
+          setNetworkState({ state: 'success', data })
+        }
+      } catch {
+        setNetworkState({ state: 'error', code: 400 })
+      }
     }
   }
+
+  const CurrentActivity = () => {
+    switch (networkState.state) {
+      case 'notAsked':
+        return (
+          <h3 className="my-4 text-3xl leading-tight">
+            Ingresa tus datos, preferencia de sesión y te contactaré
+          </h3>
+        )
+
+      case 'error':
+        return (
+          <h3 className="my-4 text-3xl leading-tight">
+            Oops, no pudimos enviar el formulario
+            <span className="px-2" role="img" aria-label="cry">
+              😢
+            </span>
+            , podrias intentar mas tarde? Gracias
+            <span className="px-2" role="img" aria-label="smiley">
+              😊
+            </span>
+          </h3>
+        )
+
+      case 'success':
+        return (
+          <h3 className="my-4 text-3xl leading-tight">
+            Información enviada!, Muchas Gracias.
+            <span className="px-2" role="img" aria-label="smiley">
+              😊
+            </span>
+          </h3>
+        )
+      case 'loading':
+        return (
+          <h3 className="my-4 text-3xl leading-tight">
+            Enviando formulario ...
+            <span className="px-2" role="img" aria-label="sending">
+              🚀
+            </span>
+          </h3>
+        )
+    }
+  }
+
   return (
     <section id="contactme" className="container mx-auto text-center py-6 mb-12">
       <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
@@ -33,51 +114,61 @@ const ContactMe: FunctionComponent = () => {
       <div className="w-full mb-4">
         <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
       </div>
-      <h3 className="my-4 text-3xl leading-tight">
-        Ingresa tus datos, preferencia de sesión y te contactaré
-      </h3>
-      <div className="text-black md:flex md:justify-center mb-6">
-        <div className="grid grid-cols gap-6">
-          <label htmlFor="Name" className="block">
-            <span className="text-gray-700">Nombre</span>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            />
-          </label>
-          <label htmlFor="email" className="block">
-            <span className="text-gray-700">Dirección de correo</span>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="mt-1 block w-full rounded-md-border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              placeholder="tu@correo.com"
-            />
-          </label>
-          <label htmlFor="yoga-option" className="text-gray-700">
-            <span className="text-gray-700">Clases</span>
-            <select
-              name="yoga-option"
-              id="yoga-option"
-              className="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            >
-              <option>Selecciona una opción</option>
-              <option>4 clases mensuales</option>
-              <option>8 clases mensuales</option>
-              <option>12 clases mensuales</option>
-            </select>
-          </label>
+      <CurrentActivity />
+      <form onSubmit={doSubmit}>
+        <div className="text-black md:flex md:justify-center mb-6">
+          <div className="grid grid-cols gap-6">
+            <label htmlFor="Name" className="block">
+              <span className="text-gray-700">Nombre</span>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required={true}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                  setName(() => event.target.value)
+                }}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </label>
+            <label htmlFor="email" className="block">
+              <span className="text-gray-700">Dirección de correo</span>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required={true}
+                onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                  setEmail(() => event.target.value)
+                }}
+                className="mt-1 block w-full rounded-md-border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                placeholder="tu@correo.com"
+              />
+            </label>
+            <label htmlFor="yoga-option" className="text-gray-700">
+              <span className="text-gray-700">Clases</span>
+              <select
+                name="yoga-option"
+                id="yoga-option"
+                onChange={optionSelected}
+                onBlur={optionSelected}
+                required={true}
+                className="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              >
+                <option value="">Selecciona una opción</option>
+                <option value="4-clases">4 clases mensuales</option>
+                <option value="8-clases">8 clases mensuales</option>
+                <option value="12-clases">12 clases mensuales</option>
+              </select>
+            </label>
+          </div>
         </div>
-      </div>
-      <button
-        onClick={doAction}
-        className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-      >
-        Enviar
-      </button>
+        <input
+          type="submit"
+          className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+          value="Enviar"
+        />
+      </form>
     </section>
   )
 }
