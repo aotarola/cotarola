03ff3302e7e9ef3a8ec938c9760c635f2237c4f8
aotarola <aotarola@groupon.com>
2021-03-15 19:35:40 -0700

    should close the ad

--- pages/components/Ad.tsx
+++ pages/components/Ad.tsx
@@ -1,18 +1,38 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 
 const Footer: FunctionComponent = () => {
+  const [shouldShow, setShouldShow] = useState(true)
+  const closeIt = (): void => {
+    setShouldShow((curr) => !curr)
+  }
   return (
-    <div className="mx-auto sm:w-3/4 md:w-2/4 fixed inset-x-0 bottom-5 gradient rounded-lg">
-      <div className="container px-8">
-        <div className="w-full flex flex-col md:flex-row ">
-          <div className="flex-1">
-            <p className="uppercase text-indigo-800 font-bold align-middle text-center py-6">
-              ¡Aprovecha el 30% de descuento solo hasta el 30 de Abril!
-            </p>
+    shouldShow && (
+      <div className="mx-auto sm:w-3/4 md:w-2/4 fixed inset-x-0 bottom-5 gradient rounded-lg">
+        <button className="absolute top-0 right-0 cursor-pointer" onClick={closeIt}>
+          <svg
+            className="h-6 w-6 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div className="container px-8">
+          <div className="w-full flex flex-col md:flex-row ">
+            <div className="flex-1">
+              <p className="uppercase text-indigo-800 font-bold align-middle text-center py-6">
+                ¡Aprovecha el 20% de descuento solo hasta el 30 de Abril!
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   )
 }
 
