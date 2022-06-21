949835809ec71f72832360f69552677a3d82db53
Andres Otarola <andres@otarola.me>
2022-06-20 22:13:24 -0700

    elm reset code

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/core.css'
-import { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
