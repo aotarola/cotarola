56eb12a5edc65ecedff67d692831d477d6cbb23f
Andres Otarola <andres@otarola.me>
2022-06-26 18:48:55 -0700

    fix formatting

--- pages/components/Testimonials.tsx
+++ pages/components/Testimonials.tsx
@@ -16,30 +16,59 @@ const Testimonials: FunctionComponent = () => {
               Andrea Flores
             </h3>
             <p className="text-primary-paragraph mb-8">
-              <em>Cuando comencé a aceptarme las cadenas que impedían mi vuelo se transformaron en alas y comencé a volar</em>
+              <em>
+                Cuando comencé a aceptarme las cadenas que impedían mi vuelo se transformaron en
+                alas y comencé a volar
+              </em>
             </p>
-            <a href="https://www.instagram.com/andrea.flores.holistica/?hl=es" className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8" target="_blank">
+            <a
+              href="https://www.instagram.com/andrea.flores.holistica/?hl=es"
+              className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8"
+              target="_blank"
+              rel="noreferrer"
+            >
               IG: @andrea.flores.holistica
             </a>
           </div>
           <div className="w-5/6 mx-auto sm:pt-10 sm:w-1/2">
-            <iframe width="450" height="315" src="https://www.youtube.com/embed/dFZJkB1Bvdw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe
+              width="450"
+              height="315"
+              src="https://www.youtube.com/embed/dFZJkB1Bvdw"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
         <div className="flex flex-wrap flex-col-reverse sm:flex-row">
           <div className="w-5/6 mx-auto sm:pt-10 sm:w-1/2">
-            <iframe width="450" height="315" src="https://www.youtube.com/embed/pqQAL2G-p9A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe
+              width="450"
+              height="315"
+              src="https://www.youtube.com/embed/pqQAL2G-p9A"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className="w-full sm:w-1/2 p-6 mt-6">
             <div className="align-middle">
               <h3 className="text-3xl text-primary-title font-bold leading-none mb-3">
                 Rosy Arrieta
               </h3>
+              <p className="text-primary-paragraph mb-8">Bioquímica</p>
               <p className="text-primary-paragraph mb-8">
-                Bioquímica
-              </p>
-              <p className="text-primary-paragraph mb-8">
-                <em>Si tuviera que elegir alguna frase sería que ‘vinimos a este mundo con la obligación de ser felices y tratando de hacer daño a la menor cantidad de personas en el camino’....lo que realmente mereces, es ser Feliz 😄💕</em>
+                <em>
+                  Si tuviera que elegir alguna frase sería que ‘vinimos a este mundo con la
+                  obligación de ser felices y tratando de hacer daño a la menor cantidad de personas
+                  en el camino’....lo que realmente mereces, es ser Feliz{' '}
+                  <span role="img" aria-label="smile and hearts">
+                    😄💕
+                  </span>
+                </em>
               </p>
             </div>
           </div>
@@ -47,38 +76,61 @@ const Testimonials: FunctionComponent = () => {
 
         <div className="flex flex-wrap">
           <div className="w-5/6 sm:w-1/2 p-6">
-            <h3 className="text-3xl text-primary-title font-bold leading-none mb-3">
-              David Lopez
-            </h3>
+            <h3 className="text-3xl text-primary-title font-bold leading-none mb-3">David Lopez</h3>
             <p className="text-primary-paragraph mb-8">
               <em>
-                Co-Fundador Escuela Creativa de Autoconocimiento (ECA), baterista en Pesticidas (Autoayuda Punk) , Empresario, Ingeniero Ambiental y Experto en Prevención de Riesgos
-                "No se Brilla sin Oscuridad " cuando te haces consciente de tu inconsciente comienza tu proceso de sanación.
-                IG: @pesticidasoficial
+                Co-Fundador Escuela Creativa de Autoconocimiento (ECA), baterista en Pesticidas
+                (Autoayuda Punk) , Empresario, Ingeniero Ambiental y Experto en Prevención de
+                Riesgos &quot;No se Brilla sin Oscuridad &quot; cuando te haces consciente de tu
+                inconsciente comienza tu proceso de sanación. IG: @pesticidasoficial
               </em>
             </p>
-            <a href="https://www.instagram.com/pesticidasoficial/?hl=es" className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8" target="_blank">
+            <a
+              href="https://www.instagram.com/pesticidasoficial/?hl=es"
+              className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8"
+              target="_blank"
+              rel="noreferrer"
+            >
               IG: @pesticidasoficial
             </a>
           </div>
           <div className="w-5/6 mx-auto sm:pt-10 sm:w-1/2">
-            <iframe width="450" height="315" src="https://www.youtube.com/embed/CsG64_MVG4A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe
+              width="450"
+              height="315"
+              src="https://www.youtube.com/embed/CsG64_MVG4A"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
         </div>
 
         <div className="flex flex-wrap flex-col-reverse sm:flex-row">
           <div className="w-5/6 mx-auto sm:pt-10 sm:w-1/2">
-            <iframe width="450" height="315" src="https://www.youtube.com/embed/KFZh8ONtpSA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe
+              width="450"
+              height="315"
+              src="https://www.youtube.com/embed/KFZh8ONtpSA"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
           <div className="w-full sm:w-1/2 p-6 mt-6">
             <div className="align-middle">
               <h3 className="text-3xl text-primary-title font-bold leading-none mb-3">
                 Martin Moreira
               </h3>
-              <p className="text-primary-paragraph mb-8">
-                Productor, Artista e Ingeniero
-              </p>
-              <a href="https://www.instagram.com/doctor.ritmo/?hl=es" className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8" target="_blank">
+              <p className="text-primary-paragraph mb-8">Productor, Artista e Ingeniero</p>
+              <a
+                href="https://www.instagram.com/doctor.ritmo/?hl=es"
+                className="inline-block mx-auto lg:mx-0 hover:underline gradient text-button-500 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out mb-8"
+                target="_blank"
+                rel="noreferrer"
+              >
                 IG: @doctor.ritmo
               </a>
             </div>
