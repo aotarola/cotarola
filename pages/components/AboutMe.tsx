801e10098cb9479264893fea4657052e15904645
Andres Otarola <andres@otarola.me>
2022-06-26 18:43:58 -0700

    fix components

--- pages/components/AboutMe.tsx
+++ pages/components/AboutMe.tsx
@@ -12,50 +12,101 @@ const Services: FunctionComponent = () => {
         </div>
         <div className="flex flex-wrap">
           <div className="w-5/6 mx-auto sm:pt-10 sm:w-1/2">
-            <img
-              className="sm:h-96 mx-auto rounded-xl"
-              alt="Quien Soy"
-              src="/quien-soy.jpg"
-            />
+            <img className="sm:h-96 mx-auto rounded-xl" alt="Quien Soy" src="/quien-soy.jpg" />
           </div>
           <div className="w-5/6 sm:w-1/2 p-6">
             <div className="p-6">
               <p className="text-primary-paragraph mb-8">
-                Mi Nombre es Claudia Otárola, químico farmacéutico de profesión con más de 10 años en el sector farmacéutico privado, nacida un 31 de Marzo de 1988 para que saques la cuenta de mi edad según el momento en que estés leyendo esto. No obstante, mi vocación de servicio ha apuntado hacia el Crecimiento, Evolución y Transformación personal aplicando los conocimientos de mi formación científica más los conocimientos holísticos que he ido aprendiendo constantemente como nutrición en mi propio camino y transformación personal para así entregarlo desde un acompañamiento amable, respetuoso, amoroso y compasivo.
-            </p>
+                Mi Nombre es Claudia Otárola, químico farmacéutico de profesión con más de 10 años
+                en el sector farmacéutico privado, nacida un 31 de Marzo de 1988 para que saques la
+                cuenta de mi edad según el momento en que estés leyendo esto. No obstante, mi
+                vocación de servicio ha apuntado hacia el Crecimiento, Evolución y Transformación
+                personal aplicando los conocimientos de mi formación científica más los
+                conocimientos holísticos que he ido aprendiendo constantemente como nutrición en mi
+                propio camino y transformación personal para así entregarlo desde un acompañamiento
+                amable, respetuoso, amoroso y compasivo.
+              </p>
               <p className="text-primary-paragraph mb-8">
-                El año 2019 tomé la decisión de profundizar en mi crecimiento personal y autoobservación y me formé como Mentora Espiritual con Paula Velez (Psicóloga, escritora del libro “¿Cómo ser una mujer plena, exitosa y feliz?” y creadora de la comunidad “Tu espiritualidad”) para acompañar procesos profundos de transformación personal, un camino recorrido junto a ella hasta inicios del 2022 donde finalicé con su formación “Warrior’s of ligth mentoring”. Durante los años 2020 y 2021 estuve inmersa en distintos tipos de formaciones de las cuales adquirí herramientas que me han ayudado a indagar en mí, descubrirme, re-programar mi mente, poner más consciencia en la sintonización de mis pensamientos y emociones y transformarme constantemente para así sostener el proceso de otros desde la teoría y la experiencia.
-            </p>
-              <p className="text-primary-paragraph mb-8">
-                En estos año me formé como:
-                <ul className="text-primary-paragraph ml-4 mb-8 list-disc">
-                  <li>
-                    Instructora de yoga 800hrs con certificación internacional en la Escuela Internacional de Loknath Yoga Chile donde además recibí mi nombre espiritual “Gayatri”, una formación basada en el yoga tradicional, su filosofía y la integración del yoga a la vida cotidiana.
-                </li>
-                  <li>
-                    Taróloga con Pedro Engel donde lo más cautivante de este curso fue darme cuenta del ciclo de mi vida a través del viaje de cada arcano, me sirvió para comprender los procesos de mi propio viaje volviéndome más consciente de cada paso que doy, comprender el porqué y para qué hago lo que hago cuando lo hago. Lo fundamental que he aplicado es el respeto de las lecturas según el mensaje que necesita escuchar la otra persona para atravesar en amor los desafíos que se presenten en su viaje y ciclo de autoconocimiento y crecimiento desde lo humano a lo espiritual y divino. Para que tengas bien claro, el tarot es una herramienta de autoconocimiento que puede darte pautas sobre lo más próximo a manifestar según los condicionamientos energéticos de tu estado presente. El tarot nos ayuda a descubrir que el futuro lo estás escribiendo en tu presente por lo tanto, lecturas predictivas y futuristas serían condicionarte a tu estado actual en vez de abrirte camino a la exploración del ser infinito que eres.
-                  </li>
-                  <li>
-                    Tomé un curso de regresiones de vida pasadas con técnicas chamánicas con Alberto Calo fundador de la Escuela de Sabiduría ancestral donde me ayudó a comprender la multidimensionalidad atemporal que estamos constantemente transitando.
-                  </li>
-                  <li>
-                    Hice un curso de Consetelaciones familiares y sanación de árbol genealógico con Jairo Velásquez Garín (Activador de Misión Planeta Arcoíris) fundamental para darle orden a mi ciclo de vida y crecimiento y así poder comprender con mayor claridad la transición que vive cada persona que llega a atenderse conmigo.
-                  </li>
-                  <li>
-                    En julio 2021 viví el retiro de sanación "Renacer Tántrico" en Costa Rica junto a Carolina Menéndez de la comunidad de @Omvocado donde viví una intensa apertura de mi chakra corazón hacia la vida, fue un momento clave para direccionar la forma en cómo estaba entregando mi servicio y en cómo me estaba integrando al llamado de mi alma en todas las áreas de mi vida.
-                  </li>
-                  <li>Soy una estudiante activa de "Un Curso de Milagros".</li>
-                  <li>
-                    Uno de mis lemas es "todo lo que leo y estudio de forma autodidacta lo implemento en mi vida, lo pruebo y me abro a compartir lo que me encuentro en mí; soy una fiel creyente que el camino hacia una verdadera transformación es explorando nuevas formas"
-                  </li>
-                </ul>
+                El año 2019 tomé la decisión de profundizar en mi crecimiento personal y
+                autoobservación y me formé como Mentora Espiritual con Paula Velez (Psicóloga,
+                escritora del libro “¿Cómo ser una mujer plena, exitosa y feliz?” y creadora de la
+                comunidad “Tu espiritualidad”) para acompañar procesos profundos de transformación
+                personal, un camino recorrido junto a ella hasta inicios del 2022 donde finalicé con
+                su formación “Warrior’s of ligth mentoring”. Durante los años 2020 y 2021 estuve
+                inmersa en distintos tipos de formaciones de las cuales adquirí herramientas que me
+                han ayudado a indagar en mí, descubrirme, re-programar mi mente, poner más
+                consciencia en la sintonización de mis pensamientos y emociones y transformarme
+                constantemente para así sostener el proceso de otros desde la teoría y la
+                experiencia.
+              </p>
+              <p className="text-primary-paragraph mb-8">En estos año me formé como:</p>
 
+              <ul className="text-primary-paragraph ml-4 mb-8 list-disc">
+                <li>
+                  Instructora de yoga 800hrs con certificación internacional en la Escuela
+                  Internacional de Loknath Yoga Chile donde además recibí mi nombre espiritual
+                  “Gayatri”, una formación basada en el yoga tradicional, su filosofía y la
+                  integración del yoga a la vida cotidiana.
+                </li>
+                <li>
+                  Taróloga con Pedro Engel donde lo más cautivante de este curso fue darme cuenta
+                  del ciclo de mi vida a través del viaje de cada arcano, me sirvió para comprender
+                  los procesos de mi propio viaje volviéndome más consciente de cada paso que doy,
+                  comprender el porqué y para qué hago lo que hago cuando lo hago. Lo fundamental
+                  que he aplicado es el respeto de las lecturas según el mensaje que necesita
+                  escuchar la otra persona para atravesar en amor los desafíos que se presenten en
+                  su viaje y ciclo de autoconocimiento y crecimiento desde lo humano a lo espiritual
+                  y divino. Para que tengas bien claro, el tarot es una herramienta de
+                  autoconocimiento que puede darte pautas sobre lo más próximo a manifestar según
+                  los condicionamientos energéticos de tu estado presente. El tarot nos ayuda a
+                  descubrir que el futuro lo estás escribiendo en tu presente por lo tanto, lecturas
+                  predictivas y futuristas serían condicionarte a tu estado actual en vez de abrirte
+                  camino a la exploración del ser infinito que eres.
+                </li>
+                <li>
+                  Tomé un curso de regresiones de vida pasadas con técnicas chamánicas con Alberto
+                  Calo fundador de la Escuela de Sabiduría ancestral donde me ayudó a comprender la
+                  multidimensionalidad atemporal que estamos constantemente transitando.
+                </li>
+                <li>
+                  Hice un curso de Consetelaciones familiares y sanación de árbol genealógico con
+                  Jairo Velásquez Garín (Activador de Misión Planeta Arcoíris) fundamental para
+                  darle orden a mi ciclo de vida y crecimiento y así poder comprender con mayor
+                  claridad la transición que vive cada persona que llega a atenderse conmigo.
+                </li>
+                <li>
+                  En julio 2021 viví el retiro de sanación &quot;Renacer Tántrico&quot; en Costa
+                  Rica junto a Carolina Menéndez de la comunidad de @Omvocado donde viví una intensa
+                  apertura de mi chakra corazón hacia la vida, fue un momento clave para direccionar
+                  la forma en cómo estaba entregando mi servicio y en cómo me estaba integrando al
+                  llamado de mi alma en todas las áreas de mi vida.
+                </li>
+                <li>Soy una estudiante activa de &quot;Un Curso de Milagros&quot;.</li>
+                <li>
+                  Uno de mis lemas es &quot;todo lo que leo y estudio de forma autodidacta lo
+                  implemento en mi vida, lo pruebo y me abro a compartir lo que me encuentro en mí;
+                  soy una fiel creyente que el camino hacia una verdadera transformación es
+                  explorando nuevas formas&quot;
+                </li>
+              </ul>
+              <p className="text-primary-paragraph mb-8">
+                La integración de todos estos conocimientos teóricos, aplicados de forma íntegra y
+                constante en cada paso que doy me llevó a autodenominarme “Terapeuta del Alma”. He
+                acompañado a más de 50 personas en sus procesos de sanación a través de mi programa
+                “Abre tus Alas”, mi máximo servicio donde trabajamos 1:1 en sesiones individuales
+                complementando con clases de yoga, regresiones a vidas pasadas y cartas oráculos.
+                Este servicio me llevó a replantear mi formato de servicio para llegar a más
+                corazones que necesitan ser escuchados y acompañados y finalizado mi retiro de
+                sanación tántrico canalicé el curso “Evolución 33”, un profundo viaje de
+                autoobservación y una tremenda invitación a recuperar tu poder de decisión, tu
+                libertad de los condicionamientos inconscientes y heredados y un completo giro hacia
+                tu brújula interior para aterrizarte en tu humanidad en conexión a tu divinidad.
               </p>
               <p className="text-primary-paragraph mb-8">
-                La integración de todos estos conocimientos teóricos, aplicados de forma íntegra y constante en cada paso que doy me llevó a autodenominarme “Terapeuta del Alma”. He acompañado a más de 50 personas en sus procesos de sanación a través de mi programa “Abre tus Alas”, mi máximo servicio donde trabajamos 1:1 en sesiones individuales complementando con clases de yoga, regresiones a vidas pasadas y cartas oráculos. Este servicio me llevó a replantear mi formato de servicio para llegar a más corazones que necesitan ser escuchados y acompañados y finalizado mi retiro de sanación tántrico canalicé el curso “Evolución 33”, un profundo viaje de autoobservación y una tremenda invitación a recuperar tu poder de decisión, tu libertad de los condicionamientos inconscientes y heredados y un completo giro hacia tu brújula interior para aterrizarte en tu humanidad en conexión a tu divinidad.
-              </p>
-              <p className="text-primary-paragraph mb-8">
-                Actualmente estoy enfocada en la creación de espacios grupales a través de CONEXIÓN PRISMA y talleres de temas específicos. Mi convicción es que acompañados la visión se hace más amplia, se conocen más caminos posibles por descubrir y podemos sostenernos y recordar desde nuestra sabiduría ancestral que somos tribu.
+                Actualmente estoy enfocada en la creación de espacios grupales a través de CONEXIÓN
+                PRISMA y talleres de temas específicos. Mi convicción es que acompañados la visión
+                se hace más amplia, se conocen más caminos posibles por descubrir y podemos
+                sostenernos y recordar desde nuestra sabiduría ancestral que somos tribu.
               </p>
             </div>
           </div>
